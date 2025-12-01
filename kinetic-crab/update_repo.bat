3cf2913465c8ebbbd08626ff5ad386731fbcbd95
Bharat Rathi <bharat.r04@gmail.com>
2025-12-01 13:12:24 -0500

    Update deployment script

--- kinetic-crab/update_repo.bat
+++ kinetic-crab/update_repo.bat
@@ -14,7 +14,7 @@ git add .
 set /p commit_msg="Enter commit message (default: Update): "
 if "%commit_msg%"=="" set commit_msg=Update
 git commit -m "%commit_msg%"
-git push
+git push -u origin main
 
 echo.
 echo Done!
